6ab7779235882ea6fc55066552f6bea5ca234a88
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-08-25 01:19:52 +0100

    Custom tailwind styling instead of base styling

--- app/noServices.tsx
+++ app/noServices.tsx
@@ -1,8 +1,7 @@
 export default function NoServices({station}: {station: string}) {
     return (
         <div className={"flex justify-center items-center h-dvh"}>
-            <div className={"flex flex-col bg-blue-950 justify-center rounded-xl px-8"}
-                 style={{width: "500px", height: "500px", filter: "drop-shadow(0 0 250px #000084ff)"}}>
+            <div className={"flex flex-col bg-blue-950 justify-center rounded-xl px-8 w-[500px] h-[500px] drop-shadow-[0_0_250px_#000084]"}>
                 <p className={"mt-2 text-3xl text-blue-400"}>No Information Available</p>
                 <p className={"mt-5 border-l-2 border-l-blue-400 pl-4 text-blue-400"}>{station} has no departures.</p>
             </div>
