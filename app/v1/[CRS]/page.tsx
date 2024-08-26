1947ad5c141a1a88558f95678e01873f1ff3de01
Arun Fletcher <focalpear00@yahoo.co.uk>
2024-08-26 14:58:37 +0100

    Remove unnecessary export

--- app/v1/[CRS]/page.tsx
+++ app/v1/[CRS]/page.tsx
@@ -8,13 +8,13 @@ const password = "6093bb063b6182aeafb406be78fee0bc34f01dfc";
 const headers = new Headers();
 headers.append('Authorization', 'Basic ' + btoa(`${username}:${password}`));
 
-export async function getData(station: string) {
+async function getData(station: string) {
     // Fetches all departures from this station up to 4 hours from now
     const response = await fetch(`https://api.rtt.io/api/v1/json/search/${station}`, {headers: headers, cache: "no-cache"});
     return await response.json();
 }
 
-export async function getServiceInfo(serviceUid: string, runDate: string) {
+async function getServiceInfo(serviceUid: string, runDate: string) {
     const d = runDate.split("-");
     const response = await fetch(`https://api.rtt.io/api/v1/json/service/${serviceUid}/${d[0]}/${d[1]}/${d[2]}`, {headers: headers, cache: "no-cache"});
     return await response.json();
